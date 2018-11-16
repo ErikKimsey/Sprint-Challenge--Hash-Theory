a016ff1a5ff0b3f35f31c2c41d989fea14441b0e
Erik Kimsey <erik@erikkimsey.com>
2018-11-16 11:48:28 -0500

    First draft of get_indices_etc

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -3,14 +3,35 @@
 #include "hashtable.h"
 #include "ex1.h"
 
+typedef struct Answer {
+  int index_1;
+  int index_2;
+} Answer;
+
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
-  HashTable *ht = create_hash_table(16);
+  HashTable *ht = create_hash_table(length);
+  Answer answ_finalle;
+  printf("cap >>>> %d", ht->capacity);
+  for(int i=0; i< ht->capacity; i++){
+    hash_table_insert(ht, i, weights[i]);
+  }
+  for (int i=0; i < ht->capacity; i++){
+    for (int j=i+1; j < ht->capacity; j++){
+      if(ht->storage[i]->value + ht->storage[j]->value == limit){
+        answ_finalle = {.index_1 = ht->storage[i]->key, .index_2 = ht->storage[j]->key};
+      }
+    }
+  }
 
-  // YOUR CODE HERE
-
-  return NULL;
+  return answ_finalle;
 }
+
+// int check_for_sum(int x, int y, int limit){
+//   if(x + y == limit){
+
+//   }
+// }
 
 void print_answer(Answer *answer)
 {
